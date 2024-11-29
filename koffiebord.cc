278e48b37ea2ac7caac227027e4f8669110ac8c3
sinaasappeltaart <s3881075@vuw.leidenuniv.nl>
2024-11-29 13:26:02 +0100

    begin funties

--- koffiebord.cc
+++ koffiebord.cc
@@ -1,11 +1,25 @@
 // file koffiebord.cc
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 #include "koffiebord.h"
 using namespace std;
 
-int leesgetal (int bovengrens) {
-  // TODO, zie Opgave 3
-}//leesgetal
+int leesgetal (int bovengrens) { //gepakt uit opdracht 3
+    int resultaat = 0;
+    char kar;
+    while (cin.get(kar) && kar != '\n') {
+        if (kar >= '0' && kar <= '9') { // Check of het karakter een cijfer is
+            resultaat = resultaat * 10 + (kar - '0'); // Bouw getal op met cijfers
+            if (resultaat > bovengrens) { // Stop als getal boven limiet komt
+                resultaat = bovengrens;
+                break;
+            }
+        }
+    }
+    return resultaat;
+}
+//leesgetal
 
 bordvakje::bordvakje ( ) {
   // TODO
@@ -23,5 +37,18 @@ void koffiebord::drukaf ( ) {
   cout << "Een koffiebord ziet er mooi uit." << endl;
   // TODO
 }//koffiebord::drukaf
+
+void koffiebord::Pointerstructuur(){
+  //TODO
+}
+void koffiebord::plaatskoffie(){
+
+}
+void koffiebord::koffieburen(){
+
+}
+void koffiebord::resetbord(){
+
+}
 
 // TODO
--- koffiebord.h
+++ koffiebord.h
@@ -1,24 +1,40 @@
 // file koffiebord.h
 
 int leesgetal (int bovengrens);
-
+//klasse voor enkel vakje op het bord
 class bordvakje {
   public:
-    bool geopend;
+    bool geopend; //is het vakje geopend?
+    bool markeer; //is het vakje gemarkeerd?
+    bool koffie; //bevat het vakje koffie?
     int aantal;           //     7 0 1
+    //voor aantal omliggende koffie?
     bordvakje* buren[8];  //     6   2
+    // pointer naar de 8 buren
     bordvakje ( );        //     5 4 3
     // TODO
 };//bordvakje
-
+//klasse voor het koffiebord
 class koffiebord {
   private:
     bordvakje* ingang;
     int hoogte, breedte;
+    void Pointerstructuur();
+    void plaatskoffie();
+    void koffieburen();
+    void resetbord();
+    // iets om vakje te selecteren met coordinaat? met pointers?
+    // iets met de buren openen als het veilig is? 
+    // iets met geschiedenis? 
     // TODO
   public:
     koffiebord ( );
     ~koffiebord ( );
     void drukaf ( );
+    void openvakje(); // functie om vakje te openen
+    void markeervakje(); // functie om vakje te markeren
+    void undo(); //zet undoen
+    bool gewonnen(); // is er gewonnen?
+    bool verloren(); //is er verloren?
     // TODO
 };//koffiebord
