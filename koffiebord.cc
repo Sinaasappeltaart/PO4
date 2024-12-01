7885f346d798153bcd8f7d639952fa6d82e54a57
sinaasappeltaart <s3881075@vuw.leidenuniv.nl>
2024-12-01 16:51:31 +0100

    ik snap het nii T-T

--- koffiebord.cc
+++ koffiebord.cc
@@ -25,21 +25,78 @@ bordvakje::bordvakje ( ) {
   // TODO
 }//bordvakje::bordvakje
 
-koffiebord::koffiebord ( ) {
-  // TODO
-}//koffiebord::koffiebord
+// Constructor van koffiebord
+koffiebord::koffiebord() : ingang(nullptr), hoogte(0), breedte(0) {
+    // Constructor-initiatie, ingaande waarde voor ingang, hoogte en breedte
+    // Andere initiaties kunnen hier worden toegevoegd, zoals de initialisatie van het bord
+}
 
 koffiebord::~koffiebord ( ) {
   // TODO
 }//koffiebord::~koffiebord
 
-void koffiebord::drukaf ( ) {
-  cout << "Een koffiebord ziet er mooi uit." << endl;
-  // TODO
-}//koffiebord::drukaf
+void koffiebord::drukaf() {
+    bordvakje* rij = ingang;  // Begin bij de bovenste rij
 
-void koffiebord::Pointerstructuur(){
-  //TODO
+    for (int i = 0; i < hoogte; ++i) {  // Loop door alle rijen
+        bordvakje* vakje = rij;  // Begin bij het eerste vakje in de rij
+        for (int j = 0; j < breedte; ++j) {  // Loop door alle kolommen
+            if (vakje != nullptr) {
+                // Als het vakje geopend is, geef dan het aantal buren weer
+                if (vakje->geopend) {
+                    cout << "[" << vakje->aantal << "] ";  // Aantal koffie in het vakje
+                } else {
+                    cout << "[ ] ";  // Leeg vakje (gesloten)
+                }
+            }
+            if (vakje->buren[2] != nullptr) {  // Beweeg naar het volgende vakje in de rij
+                vakje = vakje->buren[2];
+            }
+        }
+        cout << endl;
+        if (rij->buren[4] != nullptr) {  // Beweeg naar de volgende rij
+            rij = rij->buren[4];
+        }
+    }
+}
+
+
+void koffiebord::rits(bordvakje* boven, bordvakje* onder){
+  while(boven!= nullptr && onder!= nullptr){
+    boven -> buren[4]= onder;
+    onder -> buren[0]= boven;
+    if (boven -> buren[2] != nullptr) { // rechter buren bestaan
+    boven -> buren[3]= onder -> buren[2];//rechtsonder
+    onder -> buren[1]= boven -> buren[2];//rechtsbeneden
+    }
+    if (boven -> buren[6] !=nullptr){
+    boven -> buren[5]= onder -> buren[6];//linksonder
+    onder -> buren[7]= onder -> buren[6];//linksbenden
+    }
+    boven = boven->buren[2]; // Ga naar het volgende vakje in de rij boven
+    onder = onder->buren[2]; // Ga naar het volgende vakje in de rij onder
+  }
+}
+
+bordvakje* koffiebord::maakBovensteRij(int breedte) {
+    bordvakje* rij = new bordvakje[breedte]; // Maak een nieuwe rij
+    for (int i = 0; i < breedte; ++i) {
+        rij[i].buren[2] = (i + 1 < breedte) ? &rij[i + 1] : nullptr;  // Verbind rechter buur
+        rij[i].buren[6] = (i - 1 >= 0) ? &rij[i - 1] : nullptr;  // Verbind linker buur
+    }
+    return rij;  // Retourneer de gemaakte rij
+}
+
+void koffiebord::bouwbord() {
+    ingang = maakBovensteRij(breedte); // Maak de bovenste rij
+    bordvakje* bovensteRij = ingang;
+    bordvakje* tussenrij = nullptr;   // Declareer tussenrij lokaal
+
+    for (int j = 1; j < hoogte; ++j) {
+        tussenrij = maakBovensteRij(breedte); // Nieuwe rij maken
+        rits(bovensteRij, tussenrij);         // Verbind bovenste rij met nieuwe rij
+        bovensteRij = tussenrij;             // Ga naar de volgende rij
+    }
 }
 void koffiebord::plaatskoffie(){
 
--- koffiebord.h
+++ koffiebord.h
@@ -1,40 +1,48 @@
-// file koffiebord.h
+#ifndef KOFFIEBORD_H
+#define KOFFIEBORD_H
 
-int leesgetal (int bovengrens);
-//klasse voor enkel vakje op het bord
+// Functie om een getal in te lezen met een bovengrens
+int leesgetal(int bovengrens);
+
+// Klasse voor een enkel vakje op het bord
 class bordvakje {
-  public:
-    bool geopend; //is het vakje geopend?
-    bool markeer; //is het vakje gemarkeerd?
-    bool koffie; //bevat het vakje koffie?
-    int aantal;           //     7 0 1
-    //voor aantal omliggende koffie?
-    bordvakje* buren[8];  //     6   2
-    // pointer naar de 8 buren
-    bordvakje ( );        //     5 4 3
-    // TODO
-};//bordvakje
-//klasse voor het koffiebord
+public:
+    bool geopend;    // Is het vakje geopend?
+    bool markeer;    // Is het vakje gemarkeerd?
+    bool koffie;     // Bevat het vakje koffie?
+    int aantal;      // Aantal omliggende koffievakjes
+    bordvakje* buren[8]; // Pointer naar de 8 buren (N, NE, E, SE, S, SW, W, NW)
+
+    // Constructor voor het vakje
+    bordvakje();
+};
+
+// Klasse voor het koffiebord
 class koffiebord {
-  private:
-    bordvakje* ingang;
-    int hoogte, breedte;
-    void Pointerstructuur();
-    void plaatskoffie();
-    void koffieburen();
-    void resetbord();
-    // iets om vakje te selecteren met coordinaat? met pointers?
-    // iets met de buren openen als het veilig is? 
-    // iets met geschiedenis? 
-    // TODO
-  public:
-    koffiebord ( );
-    ~koffiebord ( );
-    void drukaf ( );
-    void openvakje(); // functie om vakje te openen
-    void markeervakje(); // functie om vakje te markeren
-    void undo(); //zet undoen
-    bool gewonnen(); // is er gewonnen?
-    bool verloren(); //is er verloren?
-    // TODO
-};//koffiebord
+private:
+    bordvakje* ingang;  // Pointer naar het begin van het bord
+    int hoogte, breedte; // Hoogte en breedte van het bord
+    bordvakje* maakBovensteRij(int breedte); // Hulpfunctie om een rij te maken
+    void rits(bordvakje* boven, bordvakje* onder); // Verbind twee rijen
+    void bouwbord();  // Bouwt het bord
+    void plaatskoffie(); // Plaatst koffie in de vakjes
+    void koffieburen();  // Handelt de koffieburen af
+    void resetbord();  // Reset het bord naar de beginstaat
+    void openvakje(bordvakje* vakje); // Functie om een vakje te openen
+    void markeervakje(bordvakje* vakje); // Functie om een vakje te markeren
+    bool checkGewonnen();  // Controleert of het spel gewonnen is
+    bool checkVerloren();  // Controleert of het spel verloren is
+
+public:
+    // Constructor en destructor van koffiebord
+    koffiebord();
+    ~koffiebord();
+    void drukaf(); // Drukt het bord af
+    void openvakje(); // Open een specifiek vakje
+    void markeervakje(); // Markeer een vakje
+    void undo(); // Undo de laatste actie
+    bool gewonnen(); // Is het spel gewonnen?
+    bool verloren(); // Is het spel verloren?
+};
+
+#endif
